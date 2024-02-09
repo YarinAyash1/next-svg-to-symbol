de2aa4ef926da3f9dc8dd92ffee0c6ece9018970
Yarin Ayash <yarinayyash@gmail.com>
2024-02-09 16:00:11 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,19 @@ const nextConfig = {
         "paths": {
             "@/components/*": ["components/*"]
         }
-    }
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+            };
+        }
+
+        return config;
+    },
 };
 
 export default nextConfig;
