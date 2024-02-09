75dfea7990f18b6e87e5977f2b64d8b57102f888
Yarin Ayash <yarinayyash@gmail.com>
2024-02-09 15:58:12 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    "compilerOptions": {
+        "baseUrl": ".",
+        "paths": {
+            "@/components/*": ["components/*"]
+        }
+    }
+};
 
 export default nextConfig;
