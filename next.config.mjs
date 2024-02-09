3fd8bbc1ad941fca81572f525ebe83297e4dba1b
Yarin Ayash <yarinayyash@gmail.com>
2024-02-09 16:01:53 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "compilerOptions": {
-        "baseUrl": ".",
-        "paths": {
-            "@/components/*": ["components/*"]
-        }
+    experimental: {
+        runtime: 'experimental-edge',
     },
     reactStrictMode: true,
     swcMinify: true,
-
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                fs: false,
-            };
-        }
-
-        return config;
-    },
 };
 
 export default nextConfig;
